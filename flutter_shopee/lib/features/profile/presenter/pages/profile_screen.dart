5dd6578423c19717a19ac7b5fc6ba57d688d9268
Win Htun Oo <winhtunoo@onenex.co>
2025-08-11 22:58:33 +0700

    build recently viewed and orders

--- flutter_shopee/lib/features/profile/presenter/pages/profile_screen.dart
+++ flutter_shopee/lib/features/profile/presenter/pages/profile_screen.dart
@@ -9,6 +9,15 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+  final List<String> recentlyViewedList = [
+    'assets/images/avatar_1.png',
+    'assets/images/avatar_2.png',
+    'assets/images/avatar_3.png',
+    'assets/images/avatar_4.png',
+    'assets/images/avatar_5.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,10 +26,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           SliverAppBar(
             automaticallyImplyLeading: false,
             pinned: true,
-            title: Padding(
-              padding: const EdgeInsets.only(left: kPaddingMedium),
-              child: _buildProfileAvatarAndButton(context),
-            ),
+            title: _buildProfileAvatarAndButton(context),
             actions: [
               _buildScanButton(context),
               _buildFilterButton(context),
@@ -28,11 +34,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ],
           ),
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Text('Hello'),
-              ],
-            ),
+            child: _buildBody(context),
           )
         ],
       ),
@@ -44,9 +46,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       children: [
         _buildProfileAvatar(),
         SizedBox(width: kPaddingSmall),
-        Expanded(
-          child: _buildMyActivityButton(context)
-        ),
+        _buildMyActivityButton(context),
       ],
     );
   }
@@ -77,19 +77,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   Widget _buildMyActivityButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size(double.infinity, 35),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50),
+    return SizedBox(
+      width: 115,
+      height: 35,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 35),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
         ),
-      ),
-      child: Text(
-        'My Activity',
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-          color: Colors.white,
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text(
+            'My Activity',
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,       
+            ),
+          ),
         ),
       ),
     );
@@ -161,6 +169,261 @@ class _ProfileScreenState extends State<ProfileScreen> {
           icon: Image.asset('assets/images/button_setting.png'),
         ),
       ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kPaddingLarge, horizontal: kPaddingMedium),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildIntro(context),
+          SizedBox(height: kPaddingSmall),
+          _buildAnnouncementCard(context),
+          SizedBox(height: kPaddingMedium),
+          _buildRecentlyViewedTitle(context),
+          SizedBox(height: kPaddingSmall),
+          _buildRecentlyViewedUsers(),
+          SizedBox(height: kPaddingXLarge),
+          _buildMyOrdersTitle(context),
+          SizedBox(height: kPaddingMedium),
+          _buildMyOrdersButtons(context),
+          SizedBox(height: kPaddingXLarge),
+          _buildStoriesTitle(context),
+          SizedBox(height: kPaddingMedium),
+          
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIntro(BuildContext context) {
+    return Text(
+      'Hello, Romina!',
+      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _buildAnnouncementCard(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(kWidgetBorderRadius),
+        color: Theme.of(context).colorScheme.surfaceContainer,
+      ),
+      width: double.infinity,
+      child: Padding(
+        padding: EdgeInsets.all(kPaddingMedium),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Announcement',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                      fontWeight: FontWeight.w400,
+                    ),
+                  )
+                ]
+              ),
+            ),
+            SizedBox(width: kPaddingLarge),
+            SizedBox(
+              width: 30,
+              height: 30,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  padding: EdgeInsets.zero,
+                ),
+                child: Icon(
+                  Icons.arrow_forward, 
+                  color: Colors.white, 
+                  size: 20
+                ),
+              ),
+            ),
+          ],
+        )
+      )
+    );
+  }
+
+  Widget _buildRecentlyViewedTitle(BuildContext context) {
+    return Text(
+      'Recently Viewed',
+      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _buildRecentlyViewedUsers() {
+    return Row(
+      children: [
+        ...recentlyViewedList.asMap().entries.expand((entry) {
+          final index = entry.key;
+          final item = entry.value;
+          return [
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(item),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.all( Radius.circular(50.0)),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 4.0,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.2),
+                        blurRadius: 8.0,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            if (index < recentlyViewedList.length - 1)
+              SizedBox(width: kPaddingSmall),
+          ];
+        })
+      ]
+    );
+  }
+
+  Widget _buildMyOrdersTitle(BuildContext context) {
+    return Text(
+      'My Orders',
+      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _buildMyOrdersButtons(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: _buildToPayButton(context),
+        ),
+        SizedBox(width: kPaddingMedium),
+        Expanded(
+          child: _buildToReceiveButton(context),
+        ),
+        SizedBox(width: kPaddingMedium),
+        Expanded(
+          child: _buildToReviewButton(context),
+        ) 
+      ],
+    );
+  }
+
+  Widget _buildToPayButton(BuildContext context) {
+    return Ink(
+      decoration: ShapeDecoration(
+        color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2), 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(kWidgetBorderRadius),
+        )
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kPaddingMedium, vertical: kPaddingSmall),
+        child: Text(
+          'To Pay',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildToReceiveButton(BuildContext context) {
+    return Stack(
+      children: [
+        Ink(
+        decoration: ShapeDecoration(
+          color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2), 
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(kWidgetBorderRadius),
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kPaddingMedium, vertical: kPaddingSmall),
+          child: Text(
+            'To Receive',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
+      ),
+      Positioned(
+        right: 10,
+        child: Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
+    ],
+    );
+  }
+
+  Widget _buildToReviewButton(BuildContext context) {
+    return Ink(
+      decoration: ShapeDecoration(
+        color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2), 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(kWidgetBorderRadius),
+        )
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kPaddingMedium, vertical: kPaddingSmall),
+        child: Text(
+          'To Review',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStoriesTitle(BuildContext context) {
+    return Text(
+      'Stories',
+      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+        fontWeight: FontWeight.w600,
+      ),  
     );
   }
 }
